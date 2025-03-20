6db08ef0f5e650095f49e7bfb656fdc5e4938657
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-03-20 14:27:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+
+const codes = document.querySelectorAll(".code");
+
+
+codes.forEach((code, i)=>{
+	code.addEventListener("input", (e) => {
+		const currentInput = e.target;
+		const nextInput = codes[i + 1];
+
+		if(currentInput && nextInput){
+			nextInput.focus();
+		};
+	});
+
+	code.addEventListener("keydown", (e) => {
+		let prevInput = codes[i - 1]; 
+		if(e.code === "Backspace" && !e.target.value && prevInput){
+			prevInput.focus();
+		}
+	});
+});
+
+function handleFocus(e){
+	
+}
